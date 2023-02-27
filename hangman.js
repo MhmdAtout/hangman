eebc91615b8a71519200a957312b141f908c089e
MhmdAtout <mohamad.atout0@icloud.com>
2023-02-27 19:15:01 +0200

    getting DOM

--- hangman.js
+++ hangman.js
@@ -1,9 +1,14 @@
 const status = document.getElementById('status');
 status.innerText = "Lets have fun :)"
+
 const random_word = document.getElementById('random_word');
+const submit_word_div = document.getElementById('submit_word_div');
+
 const input = document.getElementById('input');
+const input_div = document.getElementById('input_div');
+
 const new_game_btn = document.getElementById('new_game_btn');
-const submit = document.getElementById('submit');
+const submit_btn = document.getElementById('submit_btn');
 
 const Hangman = {
     words : ["laravel", "php", "api", "javascript", "vscode", "react", "html", "sql"] 
