c7798ae8387086c27268afd84971cafcc84c1087
MhmdAtout <mohamad.atout0@icloud.com>
2023-02-27 16:52:22 +0200

    setting game rules and random words

---
+++ hangman.js
@@ -0,0 +1,10 @@
+const Hangman = {
+    words : ["laravel", "php", "api", "javascript", "vscode", "react", "html", "sql"] 
+}
+
+const Rules = {
+    gameOff : false,
+    remainingGuesses: 5,
+    word : "",
+    trueLetters: [],
+}
