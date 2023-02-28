1e03999ca09cbe362989e8e27dbe2b878f284ed5
MhmdAtout <mohamad.atout0@icloud.com>
2023-02-28 09:20:13 +0200

    saving extention edits

--- hangman.js
+++ hangman.js
@@ -1,22 +1,31 @@
-const status = document.getElementById('status');
-status.innerText = "Lets have fun :)"
+const status = document.getElementById("status");
+status.innerText = "Lets have fun :)";
 
-const random_word = document.getElementById('random_word');
-const submit_word_div = document.getElementById('submit_word_div');
+const random_word = document.getElementById("random_word");
+const submit_word_div = document.getElementById("submit_word_div");
 
-const input = document.getElementById('input');
-const input_div = document.getElementById('input_div');
+const input = document.getElementById("input");
+const input_div = document.getElementById("input_div");
 
-const new_game_btn = document.getElementById('new_game_btn');
-const submit_btn = document.getElementById('submit_btn');
+const new_game_btn = document.getElementById("new_game_btn");
+const submit_btn = document.getElementById("submit_btn");
 
-const Hangman = {
-    words : ["laravel", "php", "api", "javascript", "vscode", "react", "html", "sql"] 
-}
+const hangman = {
+    words: [
+        "laravel",
+        "php",
+        "api",
+        "javascript",
+        "vscode",
+        "react",
+        "html",
+        "sql",
+    ],
+};
 
-const Rules = {
-    gameOff : false,
+const rules = {
+    gameOff: false,
     remainingGuesses: 5,
-    word : "",
+    word: "",
     trueLetters: [],
-}
+};
